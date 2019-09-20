1525cf98f90ce7dfe07277a056bb5632cebe8be6
lmilliken <millifly@gmail.com>
2019-09-19 20:17:39 -0600

    add jwt and POST /users

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const config = require('./config/config');
 
 require('./models/User');
@@ -10,6 +10,9 @@ const User = mongoose.model('User');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
 app.listen(PORT, () => {
   console.log(`Server up at port ${PORT}`);
 });
@@ -18,16 +21,22 @@ app.get('/api/test', (req, res) => {
   res.send("it's working");
 });
 
-app.post('/api/user/register', (req, res) => {
-  const nameFirst = request.nameFirst;
-  const nameLast = request.nameLast;
-  const email = request.email;
-  User.create(...nameFirst, nameLast, email, function(err, small) {
-    if (err) {
-      res.send(err);
-    } else {
-    }
-  });
+app.post('/api/users', (req, res) => {
+  const nameFirst = req.body.nameFirst;
+  const nameLast = req.body.nameLast;
+  const email = req.body.email;
+  const password = req.body.password;
 
-  res.send();
+  var user = new User({ nameFirst, nameLast, email, password });
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
--- models/User.js
+++ models/User.js
@@ -1,10 +1,47 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const config = require('../config/config');
 
 const UserSchema = new mongoose.Schema({
-    nameFirst: String,
-    nameLast: String,
-    email: String
-})
+  nameFirst: String,
+  nameLast: String,
+  email: String,
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+});
+
+//instance method that we call on a specifc instance of user, not the User model
+UserSchema.methods.generateAuthToken = function() {
+  var user = this;
+  var access = 'auth';
+  var token = jwt
+    .sign({ _id: user._id.toHexString(), access }, config.JWT_SECRET)
+    .toString();
+
+  user.tokens.push({ access, token });
+
+  return user.save().then(() => {
+    return token;
+  });
+};
 
 //this creates a model class
 const User = mongoose.model('User', UserSchema);
